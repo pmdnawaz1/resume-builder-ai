a9f2fed1ba8677def1bbfe232b9abd6401e5c3ef
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2025-06-01 01:01:54 +0530

    doubt 1

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -428,6 +428,7 @@ export default function Builder() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-8">
       {/* Add Toaster component */}
+      {/* what is toaster */}
       <Toaster 
         position="top-center"
         toastOptions={{
